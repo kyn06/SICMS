c6c8679828aae713c267668010136a17bd84aefc
Jhe <kwinjherane.ocareza@clsu2.edu.ph>
2026-08-18 21:23:05 +0800

    Update Database.php

--- web/config/Database.php
+++ web/config/Database.php
@@ -3,9 +3,9 @@
 class Database{
     
     private $host = "127.0.0.1";
-    private $port = 3306;
+    private $port = 3307;
     private $username= "root";
-    private $password = "1234";
+    private $password = "";
     private $database = "sicms";
     private $conn;
 
