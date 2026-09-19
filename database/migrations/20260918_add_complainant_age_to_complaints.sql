68d4ec561d663629301faa553d7ea74ff87cacb0
Jhe <kwinjherane.ocareza@clsu2.edu.ph>
2026-09-19 15:07:49 +0800

    login and database

--- database/migrations/20260918_add_complainant_age_to_complaints.sql
+++
@@ -1,3 +0,0 @@
--- Add complainant age to the complaints table for the revised complaint form.
-ALTER TABLE complaints
-    ADD COLUMN complainant_age TINYINT UNSIGNED DEFAULT NULL AFTER complainant_gender;
